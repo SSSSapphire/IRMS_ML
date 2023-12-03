10feba19a931b0e9071437d5ca98b02205166645
SSSSapphire <psnamd9600hk@gmail.com>
2023-12-04 01:31:45 +0800

    删除了不用的代码，适配py规范

--- py_workspace/KmeansDemo.py
+++
@@ -1,4 +0,0 @@
-from sklearn.cluster import KMeans
-import numpy as np
-
-kmeans = KMeans(n_clusters = 2, random_state = 0,).fit(X)
---
+++ py_workspace/MainProgram.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import pca_moudle
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import display
+from sklearn.preprocessing import StandardScaler
+
+df = pd.read_csv('X1017_C+Z2.csv',index_col="Identifier")
+print("df.shape", df.shape)
+
+print("原始数据:")
+display(df)
+
+#print("平均差与标准差")
+df_stats = df.describe().loc[['mean','std']]
+df_stats.style.format("{:.2f}")
+#display(df_stats)
+
+#归一化
+#X = df.iloc[:,1:]
+scaler = StandardScaler()
+Z_sk = scaler.fit_transform(df)
+print("归一化结果")
+display(Z_sk)
+np.savetxt("Normalazation.csv",Z_sk,'%.18e',delimiter=' ')
+
+print("PCA降维结果")
+L = pca_moudle.do_Pca(Z_sk)
+display(L)
+plt.scatter(L[:, 0], L[:, 1])
+plt.axis('equal')
+plt.show()
+
+print("Kmeans聚类")
+Kmeans = pca_moudle.do_Kmeans(L)
